f0f69eb7ab09964facc9c2554961eb40134e6824
gangzhaorige <gangzhaorige.li@gmail.com>
2022-06-09 14:49:52 -0700

    Hello Ganbold

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path_visualizer/components/home/home_view.dart';
-import 'package:statsfl/statsfl.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  // debugRepaintRainbowEnabled = true;
   SystemChrome.setPreferredOrientations(
     <DeviceOrientation>[
       DeviceOrientation.portraitUp,
     ],
   ).then(
     (_) => runApp(
-      StatsFl(
-        child: const MyApp(),
-      ),
+      const MyApp(),
     ),
   );
 }
@@ -33,6 +30,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     print('Rebuilding entire App');
     return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown,
+        },
+      ),
       home: Container(
         color: Colors.blue,
         child: const SafeArea(
